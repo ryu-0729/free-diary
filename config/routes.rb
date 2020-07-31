a0a07f50929756234b94b970081605390cff1284
Ryudai Baba <ryudaibaba@RyudainoMacBook-Pro.local>
2020-07-31 15:53:10 +0900

    add gem rinku

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  get 'diaries/index'
-  get 'diaries/show'
-  get 'diaries/new'
-  get 'diaries/edit'
   root to: 'home#top'
   devise_for :users
   resource :user, except: [:new, :create, :destroy]
+  resources :diaries
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
