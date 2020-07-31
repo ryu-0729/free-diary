7b93585dc366e11180b33e73eed35a7a02fbd2a2
Ryudai Baba <ryudaibaba@RyudainoMacBook-Pro.local>
2020-07-31 14:28:12 +0900

    bootstrap and users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root to: 'home#index'
+  root to: 'home#top'
   devise_for :users
+  resource :user, except: [:new, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
